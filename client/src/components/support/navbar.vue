42048000d52bce2c7951075a72e3e69760ac734b
ravenusmc <mcuddy77@gmail.com>
2019-03-10 15:27:08 -0400

    changed color of navbar

--- client/src/components/support/navbar.vue
+++ client/src/components/support/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-static-top">
+      <nav class="navbar navbar-expand-lg navbar-custom navbar-static-top">
         <button class="navbar-toggler" type="button" data-toggle="collapse"
         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
         aria-expanded="false" aria-label="Toggle navigation">
@@ -26,4 +26,23 @@ export default {
 </script>
 
 <style>
+/* change the background color */
+.navbar-custom {
+    background-color: #28566c;
+}
+/* change the brand and text color */
+.navbar-custom .navbar-brand,
+.navbar-custom .navbar-text {
+    color: rgba(255,255,255,.8);
+}
+/* change the link color */
+.navbar-custom .navbar-nav .nav-link {
+    color: rgba(255,255,255,.5);
+}
+/* change the color of active or hovered links */
+.navbar-custom .nav-item.active .nav-link,
+.navbar-custom .nav-item:focus .nav-link,
+.navbar-custom .nav-item:hover .nav-link {
+    color: #ffffff;
+}
 </style>
