24086155c508047de453cf43768f6fab82be7632
ravenusmc <mcuddy77@gmail.com>
2019-03-17 14:41:47 -0400

    working on datetime stamp for python code

--- server/data.py
+++ server/data.py
@@ -9,6 +9,18 @@ class Data():
         self.data = pd.read_csv('./data/dc_crime_add_vars.csv')
 
     def getMapData(self):
-        print(self.data.head())
-# test = Data()
-# test.test()
+        test = {'firstDate': '3/17/2019 12:00:00 AM', 'lastDate': '3/16/2019 12:00:00 AM', 'shift': 'DAY', 'offense': 'BURGLARY'}
+        #Converting the REPORT_DAT column into a datetime column
+        self.data['REPORT_DAT'] = pd.to_datetime(self.data['REPORT_DAT'])
+        #Converting what the user entered to proper format
+        first_time_stamp = pd.to_datetime(test['firstDate'])
+        last_time_stamp = pd.to_datetime(test['lastDate'])
+        # self.data = self.data.loc[(self.data.REPORT_DAT >= first_time_stamp) &
+        # (self.data.REPORT_DAT <= last_time_stamp), :]
+        # print(self.data)
+
+
+test = Data()
+test.getMapData()
+
+#REPORT_DAT              342867 non-null object
